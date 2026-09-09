a7b2de3499961bf87c4794de20547da0c827a49c
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-09 18:46:26 +0200

    Align diff brushes with Uno color helpers

--- src/CSharpGit.Presentation/Controls/DiffLineKindToBrushConverter.cs
+++ src/CSharpGit.Presentation/Controls/DiffLineKindToBrushConverter.cs
@@ -1,16 +1,16 @@
 using CSharpGit.Domain;
+using Microsoft.UI;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
-using Windows.UI;
 
 namespace CSharpGit.Presentation.Controls;
 
 public sealed class DiffLineKindToBrushConverter : IValueConverter
 {
-    private static readonly Brush AddedBrush = new SolidColorBrush(Color.FromArgb(38, 46, 160, 67));
-    private static readonly Brush RemovedBrush = new SolidColorBrush(Color.FromArgb(38, 210, 48, 48));
-    private static readonly Brush HeaderBrush = new SolidColorBrush(Color.FromArgb(22, 128, 128, 128));
-    private static readonly Brush TransparentBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+    private static readonly Brush AddedBrush = Brush(38, 46, 160, 67);
+    private static readonly Brush RemovedBrush = Brush(38, 210, 48, 48);
+    private static readonly Brush HeaderBrush = Brush(22, 128, 128, 128);
+    private static readonly Brush TransparentBrush = Brush(0, 0, 0, 0);
 
     public object Convert(object value, Type targetType, object parameter, string language) => value switch
     {
@@ -22,4 +22,7 @@ public sealed class DiffLineKindToBrushConverter : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, string language) =>
         throw new NotSupportedException();
+
+    private static SolidColorBrush Brush(byte alpha, byte red, byte green, byte blue) =>
+        new(ColorHelper.FromArgb(alpha, red, green, blue));
 }
