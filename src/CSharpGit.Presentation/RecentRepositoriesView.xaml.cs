e04dc09249020f562b4cd0f215d14adf1ca7659b
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-09 23:10:03 +0200

    Add recent repositories tile view

---
+++ src/CSharpGit.Presentation/RecentRepositoriesView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace CSharpGit.Presentation;
+
+public sealed partial class RecentRepositoriesView : UserControl
+{
+    public RecentRepositoriesView()
+    {
+        InitializeComponent();
+    }
+}
