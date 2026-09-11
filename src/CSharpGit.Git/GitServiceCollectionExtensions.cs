eb62a3412585ba32de20d7048f5734d5db8d5882
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-11 14:48:29 +0200

    Encapsulate Git service composition

---
+++ src/CSharpGit.Git/GitServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+using CSharpGit.Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CSharpGit.Git;
+
+public static class GitServiceCollectionExtensions
+{
+    public static IServiceCollection AddCSharpGitGit(
+        this IServiceCollection services,
+        GitCliOptions? options = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddSingleton(options ?? new GitCliOptions());
+        services.AddSingleton(provider =>
+            new GitCommandExecutor(provider.GetRequiredService<GitCliOptions>()));
+
+        services.AddSingleton(provider =>
+            new GitCliRepositoryService(provider.GetRequiredService<GitCommandExecutor>()));
+        services.AddSingleton<IRepositoryService>(provider => provider.GetRequiredService<GitCliRepositoryService>());
+        services.AddSingleton<IRepositoryStateService>(provider => provider.GetRequiredService<GitCliRepositoryService>());
+        services.AddSingleton<IWorkingTreeService>(provider => provider.GetRequiredService<GitCliRepositoryService>());
+        services.AddSingleton<IWorkingTreeDiffService>(provider => provider.GetRequiredService<GitCliRepositoryService>());
+        services.AddSingleton<IReferenceService>(provider => provider.GetRequiredService<GitCliRepositoryService>());
+
+        services.AddSingleton<IRepositoryFileVersionService>(provider =>
+            new GitRepositoryFileVersionService(provider.GetRequiredService<GitCommandExecutor>()));
+
+        services.AddSingleton(provider =>
+            new GitReferenceHistoryService(provider.GetRequiredService<GitCommandExecutor>()));
+        services.AddSingleton(provider =>
+            new GitFileAwareHistoryService(
+                provider.GetRequiredService<GitReferenceHistoryService>(),
+                provider.GetRequiredService<GitCommandExecutor>()));
+        services.AddSingleton<IHistoryService>(provider => provider.GetRequiredService<GitFileAwareHistoryService>());
+        services.AddSingleton<IReferenceHistoryService>(provider => provider.GetRequiredService<GitFileAwareHistoryService>());
+
+        return services;
+    }
+}
