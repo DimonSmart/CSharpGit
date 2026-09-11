5c0c083494daad1a9381eefd8b41fe6fbe4f848d
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-11 14:49:05 +0200

    Remove repository-local Git runner

--- src/CSharpGit.Git/GitCliRepositoryService.ProcessRunner.cs
+++
@@ -1,9 +0,0 @@
-namespace CSharpGit.Git;
-
-public sealed partial class GitCliRepositoryService
-{
-    private GitProcessRunner? _sharedProcessRunner;
-
-    private GitProcessRunner SharedProcessRunner =>
-        _sharedProcessRunner ??= new GitProcessRunner(_gitExecutable);
-}
