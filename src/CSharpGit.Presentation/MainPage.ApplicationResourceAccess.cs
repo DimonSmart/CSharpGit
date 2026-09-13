147126525f29854af235e831f45c185609dad199
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-13 22:52:53 +0200

    Disambiguate MainPage XAML Application lookup

---
+++ src/CSharpGit.Presentation/MainPage.ApplicationResourceAccess.cs
@@ -0,0 +1,9 @@
+namespace CSharpGit.Presentation;
+
+public sealed partial class MainPage
+{
+    private static class Application
+    {
+        internal static Microsoft.UI.Xaml.Application Current => Microsoft.UI.Xaml.Application.Current;
+    }
+}
