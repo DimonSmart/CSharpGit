f974bf1f6f06c1ded31d0db8f9852f8366081741
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-15 15:22:28 +0200

    Constrain commit details to viewport width

--- src/CSharpGit.Presentation/MainPage.CommitCopy.cs
+++ src/CSharpGit.Presentation/MainPage.CommitCopy.cs
@@ -7,6 +7,7 @@ namespace CSharpGit.Presentation;
 public sealed partial class MainPage
 {
     private bool _commitDetailsSurfaceInitialized;
+    private CommitDetailsView? _commitDetailsView;
 
     private void InitializeCommitDetailsSurface()
     {
@@ -16,10 +17,23 @@ public sealed partial class MainPage
         DetailsScroller.HorizontalScrollMode = ScrollMode.Disabled;
         DetailsScroller.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
         DetailsScroller.HorizontalContentAlignment = HorizontalAlignment.Stretch;
-        DetailsScroller.Content = new CommitDetailsView
+
+        _commitDetailsView = new CommitDetailsView
         {
             DataContext = _viewModel,
             HorizontalAlignment = HorizontalAlignment.Stretch
         };
+        DetailsScroller.Content = _commitDetailsView;
+        DetailsScroller.SizeChanged += DetailsScroller_SizeChanged;
+        ConstrainCommitDetailsToViewport(DetailsScroller.ActualWidth);
+    }
+
+    private void DetailsScroller_SizeChanged(object sender, SizeChangedEventArgs args) =>
+        ConstrainCommitDetailsToViewport(args.NewSize.Width);
+
+    private void ConstrainCommitDetailsToViewport(double availableWidth)
+    {
+        if (_commitDetailsView is null || availableWidth <= 0) return;
+        _commitDetailsView.Width = availableWidth;
     }
 }
