29d223061dfeab9c735d1801597a68a9896ba490
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-16 22:34:25 +0200

    Select working tree preview synchronously on open

--- src/CSharpGit.Presentation/MainPage.WorkingTreeDiff.cs
+++ src/CSharpGit.Presentation/MainPage.WorkingTreeDiff.cs
@@ -43,7 +43,7 @@ public sealed partial class MainPage
         _viewModel.Changes.CollectionChanged += WorkingTreeChangesCollectionChanged;
         WorkingTreePane.RegisterPropertyChangedCallback(VisibilityProperty, (_, _) =>
         {
-            if (WorkingTreePane.Visibility == Visibility.Visible) QueueWorkingTreePreviewRestore();
+            if (WorkingTreePane.Visibility == Visibility.Visible) EnsureWorkingTreeActivePreview();
         });
 
         RebuildWorkingTreeTrees();
