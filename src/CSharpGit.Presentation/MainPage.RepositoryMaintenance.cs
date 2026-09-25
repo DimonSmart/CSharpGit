ee21ffce77dfbc25cc986a967a96eaa0b0c07adb
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-09-25 13:04:30 +0200

    Initialize History performance diagnostics

--- src/CSharpGit.Presentation/MainPage.RepositoryMaintenance.cs
+++ src/CSharpGit.Presentation/MainPage.RepositoryMaintenance.cs
@@ -78,6 +78,7 @@ public sealed partial class MainPage
         UpdateOptimizeRepositoryAvailability();
         InitializeRepositoryChangeMonitoring();
         InitializeRecentRepositories();
+        InitializeHistoryPerformanceDiagnostics();
         InitializeGitConsole();
         InitializeWorktreeSupport();
     }
